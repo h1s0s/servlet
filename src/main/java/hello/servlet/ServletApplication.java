f4bc6d8a9963a814c2c4ee892ddb0e57abbff580
한상선 <hs94122@naver.com>
2022-11-19 14:20:49 +0900

    [한상선] 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 - Hello 서블릿까지

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //스프링이 내장된 서블릿을 찾아서 자동으로 등록해주는데 서블릿을 선언해주는 어노테이션
 @SpringBootApplication
 public class ServletApplication {
 
